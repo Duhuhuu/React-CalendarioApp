b664567baf13074eeead88b1bdcd757531bfb0ac
Duhuhuu <ignisxmizu@gmail.com>
2022-10-18 13:47:16 -0600

    Start CalendarPage - diseño, rutas y login

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,24 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { LoginPage } from "../auth";
+import { CalendarPage } from "../calendar";
+
+export const AppRouter = () => {
+    
+    const authSatus= 'not-authenticated';
+  return (
+    <Routes>
+        { 
+            (authSatus === 'not-authenticated' )
+            ? <Route path="/auth/*" element={ <LoginPage />  } />
+            : <Route path="/*" element={ <CalendarPage />  } />
+            
+        }
+      
+        <Route path="/*" element={ <Navigate to="/auth/login" />  } />
+
+    </Routes>
+
+  )
+}
+
+
